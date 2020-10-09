77b8ff9755616a5c5901ca2722c4ecb2c4310c10
Righthand5 <xyx1530966653@163.com>
2020-10-09 15:25:48 +0800

    "first"

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <input v-model="message" placeholder="edit me">
+    <p>Message is: {{ message }}</p>
+    <p><button @click="message='xyx'">set message to xyx</button></p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      message:'hi'
+    }
+  },
+  components:{}
+};
+</script>
